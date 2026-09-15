b0b90db56c90fa238e64944af346beeefd03dcde
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-15 17:25:52 +0600

    Add DNS mesh state store

---
+++ backend/dns_mesh_store.go
@@ -0,0 +1,8 @@
+package backend
+
+import "sync"
+
+type DNSMeshStore struct { mu sync.RWMutex; status DNSMeshStatus }
+func NewDNSMeshStore() *DNSMeshStore { return &DNSMeshStore{status: DNSMeshStatus{Zones: []string{"familytimenet.com","ftnddns.net"}, DNSSEC:true}} }
+func (s *DNSMeshStore) Get() DNSMeshStatus { s.mu.RLock(); defer s.mu.RUnlock(); return s.status }
+func (s *DNSMeshStore) Set(v DNSMeshStatus) { s.mu.Lock(); s.status=v; s.mu.Unlock() }
